c1f46d76da140ad712fb80f52f3103ebba7f717d
emrosas <emireles.rosas@gmail.com>
2022-08-31 14:09:20 -0500

    Progress on About section.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     
     <!--Hero Text and Button-->
 
-    <div class="px-5vw pt-20 pb-10">
+    <div class="px-5vw pt-20 pb-10 border-solid border-b-2 border-dark">
         <h1 class="font-title font-extrabold text-3xl leading-normal mb-6">We help business owners build their brands</h1>
         <p class="font-body font-normal leading-loose mb-9">By designing a cohesive Branding Identity and web experiences that help them connect with their customers.</p>
         <button class="bg-buzz font-title px-5 py-1 border-solid border-2 border-dark shadow-brutal hover:shadow-hovered transition-shadow">Get in Touch!</button>
@@ -15,20 +15,24 @@
     <!--Hero Image-->
 
     <div class="bg-pink">
-        <img src="/images/rosa.png" alt="Sección Rosa" class="w-full border-solid border-dark border-2">
-    </div>
-
-</section>  
-    
-<!--About Section-->
-
-<section>
-
-    <div class="px-5vw">
-        <h1 class="font-title font-extrabold text-3xl leading-normal mb-6">We are a multidisciplinary Design Studio</h1>
+        <img src="/images/rosa.png" alt="Sección Rosa" class="w-full">
     </div>
 
 </section>
+    
+<!--About Section-->
+
+<section class="border-solid border-2 border-dark">
+
+    <div class="px-5vw py-20">
+        <h1 class="font-title font-extrabold text-3xl leading-normal mb-10">We are a multidisciplinary Design Studio</h1>
+        <p class="font-body text-lg leading-relaxed mb-4">We specialize in the creation and optimization of <b class="text-pink font-medium">Brand Identities</b> and <b class="text-pink font-medium">User Experiences</b>.</p>
+        <p class="font-body text-lg leading-loose">We aim to <b class="text-pink font-medium">Help Brands</b> tell creative stories and expand  through <b class="text-pink font-medium">Unique Design Solutions</b>.</p>
+    </div>
+
+</section>
+
+
 
 
     
