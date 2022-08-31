fb130a5c3034afe1990857cc31c3d87934582f0f
emrosas <emireles.rosas@gmail.com>
2022-08-31 13:43:09 -0500

    Changes on hero structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 
+<!--Hero Section-->
 
-
-<section class="py-20 border-solid border-l-2 border-r-2 border-dark">
+<section class="border-solid border-l-2 border-r-2 border-dark">
     
     <!--Hero Text and Button-->
 
-    <div class="px-5vw">
+    <div class="px-5vw pt-20 pb-10">
         <h1 class="font-title font-extrabold text-3xl leading-normal mb-6">We help business owners build their brands</h1>
         <p class="font-body font-normal leading-loose mb-9">By designing a cohesive Branding Identity and web experiences that help them connect with their customers.</p>
         <button class="bg-buzz font-title px-5 py-1 border-solid border-2 border-dark shadow-brutal hover:shadow-hovered transition-shadow">Get in Touch!</button>
-        <div class="w-full h-40 mt-20 mb-12" style="background-image: radial-gradient(circle at 2px 2px, black 2px, transparent 0); background-size: 35px 35px;"></div>
+        <div class="w-full h-40 mt-20" style="background-image: radial-gradient(circle at 2px 2px, black 2px, transparent 0); background-size: 35px 35px;"></div>
     </div>
     
     <!--Hero Image-->
@@ -17,7 +17,19 @@
     <div class="bg-pink">
         <img src="/images/rosa.png" alt="Sección Rosa" class="w-full border-solid border-dark border-2">
     </div>
-    
-    <div></div>
 
-</section>``
+</section>  
+    
+<!--About Section-->
+
+<section>
+
+    <div class="px-5vw">
+        <h1 class="font-title font-extrabold text-3xl leading-normal mb-6">We are a multidisciplinary Design Studio</h1>
+    </div>
+
+</section>
+
+
+    
+
