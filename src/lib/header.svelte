b1410d3c4c96b642c229c5c9863e26c85c667116
emrosas <emireles.rosas@gmail.com>
2022-09-17 19:14:33 -0500

    Added border to the left side of the Header button

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -2,7 +2,7 @@
 <header class="bg-light border-solid border-2 border-dark pl-5vw h-[8vh] flex justify-between items-center fixed left-0 right-0 z-50">
     
     <img src="/logorilla.png" alt="Logo" class=" h-4/5 ">
-    <button class="bg-dark text-white h-full px-5 font-title hover:bg-pink transition-colors">Contact Us
+    <button class="bg-dark text-white border-dark border-l-2 h-full px-5 font-title hover:bg-pink transition-colors">Contact Us
     </button>
 
 </header>
